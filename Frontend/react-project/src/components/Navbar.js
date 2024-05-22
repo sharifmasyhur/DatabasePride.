c542f2089e2c8611dd89780405311c3549b50924
sharifmasyhur <sharifalhadyyad@gmail.com>
2024-05-22 20:51:04 +0700

    Modified Frontend and Docs

---
+++ Frontend/react-project/src/components/Navbar.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+
+function Navbar() {
+    return (
+        <div className="navbar">
+            <div className="leftside">
+                <img src={Logo} />
+            </div>
+            <div className="rightSide"></div>
+        </div>
+    );
+}
+
