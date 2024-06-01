4d82baf16c6be2024d1b56f2bc05fb22f0bb9b5b
sharifmasyhur <sharifalhadyyad@gmail.com>
2024-06-02 02:38:22 +0700

    Redemption

---
+++ Frontend/src/helpers/MenuList.js
@@ -0,0 +1,52 @@
+import Pepperoni from "../assets/pepperoni.jpg";
+import Margherita from "../assets/margherita.jpg";
+import PedroTechSpecial from "../assets/pedrotechspecial.jpg";
+import Vegan from "../assets/vegan.jpg";
+import Pineapple from "../assets/pineapple.jpg";
+import Expensive from "../assets/expensive.jpg";
+import MieGoreng from "../assets/MieGoreng.jpg";
+import NasiSpesial from "../assets/NasiGorengSpesial.png";
+import Kwetiaw from "../assets/Kwetiaw.jpg";
+
+export const MenuList = [
+  {
+    name: "Pepperoni Pizza",
+    image: Pepperoni,
+    price: 20,
+  },
+  {
+    name: "Margherita Pizza",
+    image: Margherita,
+    price: 18,
+  },
+  {
+    name: "Vegan Pizza",
+    image: Vegan,
+    price: 25,
+  },
+  {
+    name: "Pineapple Pizza",
+    image: Pineapple,
+    price: 22,
+  },
+  {
+    name: "Very Expensive Pizza",
+    image: Expensive,
+    price: 35,
+  },
+  {
+    name: "Nasi Goreng Spesial 1",
+    image: NasiSpesial,
+    price: 19,
+  },
+  {
+    name: "Mie Goreng",
+    image: MieGoreng,
+    price: 15,
+  },
+  {
+    name: "Kwetiaw Goreng",
+    image: Kwetiaw,
+    price: 18,
+  },
+];
---
+++ Frontend/src/pages/Home.js
@@ -0,0 +1,20 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import BannerImage from "../assets/QuickCanteenLogo.png";
+import "../styles/Home.css";
+
+function Home() {
+  return (
+    <div className="home" style={{ backgroundImage: `url(${BannerImage})` }}>
+      <div className="headerContainer">
+        <h1> QuickCanteen </h1>
+        <p> Order Anywhere With Ease. </p>
+        <Link to="/menu">
+          <button> ORDER NOW </button>
+        </Link>
+      </div>
+    </div>
+  );
+}
+
+export default Home;
