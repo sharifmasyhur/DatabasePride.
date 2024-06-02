98f2c9403cce83b42aa544a6429cd1c3d12b97f7
sharifmasyhur <sharifalhadyyad@gmail.com>
2024-06-02 20:51:17 +0700

    edit frontend

--- Frontend/src/pages/About.js
+++ Frontend/src/pages/About.js
@@ -9,21 +9,12 @@ function About() {
         style={{ backgroundImage: `url(${MultiplePizzas})` }}
       ></div>
       <div className="aboutBottom">
-        <h1> ABOUT US</h1>
+        <h1> ABOUT US </h1>
         <p>
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime
-          mollitia, molestiae quas vel sint commodi repudiandae consequuntur
-          voluptatum laborum numquam blanditiis harum quisquam eius sed odit
-          fugiat iusto fuga praesentium optio, eaque rerum! Provident similique
-          accusantium nemo autem. Veritatis obcaecati tenetur iure eius earum ut
-          molestias architecto voluptate aliquam nihil, eveniet aliquid culpa
-          officia aut! Impedit sit sunt quaerat, odit, tenetur error, harum
-          nesciunt ipsum debitis quas aliquid. Reprehenderit, quia. Quo neque
-          error repudiandae fuga? Ipsa laudantium molestias eos sapiente
-          officiis modi at sunt excepturi expedita sint? Sed quibusdam
-          recusandae alias error harum maxime adipisci amet laborum.
-          Perspiciatis minima nesciunt dolorem! Officiis iure rerum voluptates a
-          cumque velit
+        Proyek manajemen database ini bertujuan untuk menghilangkan antrian panjang, 
+        menghemat waktu mahasiswa antar kelas atau saat istirahat. 
+        mengembangkan situs web kantin kampus yang ramah pengguna yang 
+        memungkinkan mahasiswa memesan makanan secara online dimanapun dan mengambilnya dengan nyaman tanpa perlu mengantri lama-lama.
         </p>
       </div>
     </div>
